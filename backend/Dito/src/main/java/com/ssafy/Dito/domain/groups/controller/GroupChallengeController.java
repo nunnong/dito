088ec5d37ff4166adaff7b195838a3c107b902f8
DESKTOP-HA97GMI\SSAFY <ay0109001@gmail.com>
2025-10-30 10:36:50 +0900

    fix : GroupChallenge 관련 응답 객체 SingleResult 에서 ResponseEntity로 수정

--- backend/Dito/src/main/java/com/ssafy/Dito/domain/groups/controller/GroupChallengeController.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/groups/controller/GroupChallengeController.java
@@ -1,18 +1,17 @@
 package com.ssafy.Dito.domain.groups.controller;
 
-import com.ssafy.Dito.domain.groups.dto.request.CreateGroupChallengeRequest;
-import com.ssafy.Dito.domain.groups.dto.response.GroupChallengeResponse;
+import com.ssafy.Dito.domain.groups.dto.request.CreateGroupChallengeReq;
+import com.ssafy.Dito.domain.groups.dto.response.GroupChallengeRes;
 import com.ssafy.Dito.domain.groups.service.GroupChallengeService;
 import com.ssafy.Dito.global.dto.ApiResponse;
 import com.ssafy.Dito.global.dto.SingleResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,12 +22,11 @@ public class GroupChallengeController {
     private final GroupChallengeService groupChallengeService;
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public SingleResult<GroupChallengeResponse> createGroupChallenge(
-        @Valid @RequestBody CreateGroupChallengeRequest request,
+    public ResponseEntity<SingleResult<GroupChallengeRes>> createGroupChallenge(
+        @Valid @RequestBody CreateGroupChallengeReq request,
         @RequestHeader("X-User-Id") Long userId // TODO: JWT 인증 구현 후 @AuthenticationPrincipal로 변경
     ) {
-        GroupChallengeResponse response = groupChallengeService.createGroupChallenge(request, userId);
-        return ApiResponse.ok(response);
+        GroupChallengeRes response = groupChallengeService.createGroupChallenge(request, userId);
+        return ApiResponse.create(response);
     }
 }
