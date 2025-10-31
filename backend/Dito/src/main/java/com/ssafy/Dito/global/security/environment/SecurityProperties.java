122e402eb114b2133cb0694b6836fe0138d553c8
wijihooooon <dnlwlgns1117@naver.com>
2025-11-01 07:39:01 +0900

    feat: security 설정 파일 추가 및 중복 파일 삭제

--- backend/Dito/src/main/java/com/ssafy/Dito/global/security/environment/SecurityProperties.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/global/security/environment/SecurityProperties.java
@@ -3,6 +3,27 @@ package com.ssafy.Dito.global.security.environment;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "security")
-public record SecurityProperties() {
+public record SecurityProperties(
+        JwtProperties jwt
+) {
 
+    public record JwtProperties(
+            AccessProperties access,
+            RefreshProperties refresh
+    ) {
+
+        public record AccessProperties(
+                String secret,
+                int expiration
+        ) {
+
+        }
+
+        public record RefreshProperties(
+                String secret,
+                int expirationDefault
+        ) {
+
+        }
+    }
 }
