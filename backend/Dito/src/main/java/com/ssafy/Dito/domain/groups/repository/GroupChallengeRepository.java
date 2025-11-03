8a9ac102ef576f6a575960ff629075f0c2db9a59
DESKTOP-HA97GMI\SSAFY <ay0109001@gmail.com>
2025-11-03 11:40:02 +0900

    feat : 그룹 참여하기

--- backend/Dito/src/main/java/com/ssafy/Dito/domain/groups/repository/GroupChallengeRepository.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/groups/repository/GroupChallengeRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.Dito.domain.groups.repository;
 
 import com.ssafy.Dito.domain.groups.entity.GroupChallenge;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface GroupChallengeRepository extends JpaRepository<GroupChallenge, Long> {
 
     boolean existsByInviteCode(String inviteCode);
+
+    Optional<GroupChallenge> findByInviteCode(String inviteCode);
 }
