05a312c6465373663fd8952ec9b8716b75635ddf
wijihooooon <dnlwlgns1117@naver.com>
2025-11-03 21:47:15 +0900

    fix: dto 변수명 수정

--- backend/Dito/src/main/java/com/ssafy/Dito/domain/log/mediaSessionEvent/dto/request/MediaSessionEventBatchReq.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/log/mediaSessionEvent/dto/request/MediaSessionEventBatchReq.java
@@ -3,7 +3,7 @@ package com.ssafy.Dito.domain.log.mediaSessionEvent.dto.request;
 import java.util.List;
 
 public record MediaSessionEventBatchReq(
-    List<MediaSessionEventReq> mediaSessionEvents
+    List<MediaSessionEventReq> mediaSessionEvent
 ) {
 
 }
