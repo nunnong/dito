12fea6f7e6a6f8e7741fd0ba951a2963dcf152d4
DESKTOP-HA97GMI\SSAFY <ay0109001@gmail.com>
2025-11-03 11:38:23 +0900

    feat : GroupParticipant entity, repository 생성

---
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/groups/repository/GroupParticipantRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.Dito.domain.groups.repository;
+
+import com.ssafy.Dito.domain.groups.entity.GroupChallenge;
+import com.ssafy.Dito.domain.groups.entity.GroupParticipant;
+import com.ssafy.Dito.domain.groups.entity.GroupParticipant.GroupParticipantId;
+import com.ssafy.Dito.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupParticipantRepository extends JpaRepository<GroupParticipant, GroupParticipantId> {
+
+    boolean existsByIdUserAndIdGroup(User user, GroupChallenge group);
+}
