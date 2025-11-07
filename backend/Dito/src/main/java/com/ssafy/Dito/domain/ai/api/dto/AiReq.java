27195071c5e198a04cf2529b3bc4964b2c4c3489
nicecorntea <nicecorntea@naver.com>
2025-11-07 14:51:54 +0900

    add: add JSON property annotations for userId in AiReq, mission fields in MissionReq, and missionId in MissionResultReq

--- backend/Dito/src/main/java/com/ssafy/Dito/domain/ai/api/dto/AiReq.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/ai/api/dto/AiReq.java
@@ -1,6 +1,9 @@
 package com.ssafy.Dito.domain.ai.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record AiReq(
+    @JsonProperty("user_id")
     long userId
 ) {
 
--- backend/Dito/src/main/java/com/ssafy/Dito/domain/missionResult/dto/request/MissionResultReq.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/missionResult/dto/request/MissionResultReq.java
@@ -1,9 +1,12 @@
 package com.ssafy.Dito.domain.missionResult.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.Dito.domain.missionResult.entity.Result;
 
 public record MissionResultReq(
+        @JsonProperty("mission_id")
         long missionId,
+
         Result result
 ) {
 
