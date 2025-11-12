d90cad359606755751058c8d6d9f991d9a7d2fa7
yunyoung <juyy99@naver.com>
2025-11-12 20:20:20 +0900

    git pull origin android

--- android/app/src/main/java/com/dito/app/feature/group/GroupScreen.kt
+++ android/app/src/main/java/com/dito/app/feature/group/GroupScreen.kt
@@ -24,13 +24,10 @@ import kotlinx.coroutines.delay
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
