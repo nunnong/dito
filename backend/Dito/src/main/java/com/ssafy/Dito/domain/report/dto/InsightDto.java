280396e39355cbdd6f8d72b15b1f143a0614445f
nicecorntea <nicecorntea@naver.com>
2025-11-19 14:49:38 +0900

    feat: add score field

--- backend/Dito/src/main/java/com/ssafy/Dito/domain/report/dto/InsightDto.java
+++ backend/Dito/src/main/java/com/ssafy/Dito/domain/report/dto/InsightDto.java
@@ -3,12 +3,15 @@ package com.ssafy.Dito.domain.report.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.Map;
 
 public record InsightDto(
     @NotNull(message = "Insight type is required")
     InsightType type,
 
     @NotBlank(message = "Insight description is required")
-    String description
+    String description,
+
+    Map<String, Integer> score
 ) {
 }
